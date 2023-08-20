13baba7bbf5f9de9ad28f698f90e50a52e439935
Ruma543 <ruma.afrin543@gmail.com>
2023-08-20 20:54:52 +0600

    some changes are added

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let count = 0;
 let totalPrice = 0;
-// step-1
 function handleClick(target) {
-// step-2
   const itemContainer = document.getElementById("product-container")
   const item = target.childNodes[5].childNodes[1].innerText;
   const p = document.createElement("p");
@@ -14,7 +12,6 @@ function handleClick(target) {
   totalPrice = parseFloat(price) + parseFloat(totalPrice);
   document.getElementById("total-price").innerText = totalPrice.toFixed(2);
   
-  
   const purchaseBtn = document.getElementById("purchase-btn");
   if (totalPrice > 0) {
     purchaseBtn.removeAttribute("disabled");
@@ -22,39 +19,49 @@ function handleClick(target) {
      purchaseBtn.setAttribute("disabled", true);
    }
    total = totalPrice;
-    document.getElementById("total").innerText = total;
+  document.getElementById("total").innerText = total;
 
-  // step-4
   const couponBtn = document.getElementById("coupon-btn");
-   const couponCodeElement = document.getElementById("coupon-code");
+  const couponCodeElement = document.getElementById("coupon-code");
   const couponCode = couponCodeElement.innerText;
-  
    if (totalPrice >= 200) {
      couponBtn.removeAttribute("disabled");
   } else {
      couponBtn.setAttribute("disabled", true);
   } 
-  
 }
 function clicked() {
-      const couponInput = document.getElementById("coupon-input");
-      const couponValue = couponInput.value;
+  const couponInput = document.getElementById("coupon-input");
+  const couponValue = couponInput.value;
 
    if (couponValue === "SELL200") { 
         const discountPrice = totalPrice * 0.2;
         const total = totalPrice - discountPrice;
-        
         document.getElementById("total-price").innerText = totalPrice.toFixed(2);
-        
         document.getElementById("discount-price").innerText = discountPrice.toFixed(2);
-      document.getElementById("total").innerText = total.toFixed(2);
-      couponInput.value = "";
+        document.getElementById("total").innerText = total.toFixed(2);
+        couponInput.value = "";
     } else {
-      alert("Please input valid coupone code");
-      couponInput.value = "";
+       alert("Please input valid coupone code");
+       couponInput.value = "";
    }
 } 
-function reloaded() {
-   window.location.href = "index.html";
+function reloadValues() {
+  let count = 0;
+  let totalPrice = 0;
+  document.getElementById("total-price").innerText = "0.00";
+  document.getElementById("discount-price").innerText = "0.00";
+  document.getElementById("total").innerText = "0.00";
+  const itemContainer = document.getElementById("product-container");
+  itemContainer.innerHTML = "";
+  const purchaseBtn = document.getElementById("purchase-btn");
+  purchaseBtn.setAttribute("disabled", true);
+  
+  const couponBtn = document.getElementById("coupon-btn");
+  couponBtn.setAttribute("disabled", true);
+
 }
-// window.location.href = "bank.html";
+document.getElementById("reset").addEventListener("click", function() {
+   reloadValues();
+  
+});
